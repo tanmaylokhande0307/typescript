2a7cf551bcf01f43945057eb76e8311fc71e6faa
Tanmay <tl@zasmlabs.com>
2024-05-29 02:20:26 +0530

    typescript arrays

---
+++ sum.js
@@ -0,0 +1,5 @@
+"use strict";
+const getFirstElement = (arr) => {
+    return arr[0];
+};
+console.log(getFirstElement([1, 2, 3, 5]));
---
+++ sum.ts
@@ -0,0 +1,5 @@
+const getFirstElement = (arr: number[]): number => {
+  return arr[0]
+}
+
+console.log(getFirstElement([1, 2, 3, 5]))
